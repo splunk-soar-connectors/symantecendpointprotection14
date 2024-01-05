632531c48dba3dcabd7f25a162347cdea4a945fa
mishalp-crest <mishalp@splunk.com>
2024-01-05 15:15:38 +0530

    make variable consistent

--- sep14_connector.py
+++ sep14_connector.py
@@ -50,7 +50,7 @@ ERROR_RESPONSE_DICT = {
     consts.SEP_REST_RESP_UNAUTHORIZED: consts.SEP_REST_RESP_UNAUTHORIZED_MSG,
     consts.SEP_REST_RESP_BAD_REQUEST: consts.SEP_REST_RESP_BAD_REQUEST_MSG,
     consts.SEP_REST_RESP_NOT_FOUND: consts.SEP_REST_RESP_NOT_FOUND_MSG,
-    consts.SEP_REST_RESP_ERROR_IN_PROCESSING: consts.SEP_REST_RESP_ERROR_IN_PROCESSING_MSG,
+    consts.SEP_REST_RESP_ERR_IN_PROCESSING: consts.SEP_REST_RESP_ERR_IN_PROCESSING_MSG,
     consts.SEP_REST_RESP_FORBIDDEN: consts.SEP_REST_RESP_FORBIDDEN_MSG,
     consts.SEP_REST_RESP_GONE: consts.SEP_REST_RESP_GONE_MSG
 }
@@ -314,7 +314,7 @@ class Sep14Connector(BaseConnector):
             return phantom.APP_SUCCESS, response_data
 
         # If response code is unknown
-        message = consts.SEP_REST_RESP_OTHER_ERROR_MSG
+        message = consts.SEP_REST_RESP_OTHER_ERR_MSG
 
         # overriding message if available in response
         if isinstance(response_data, dict):
@@ -494,7 +494,7 @@ class Sep14Connector(BaseConnector):
             # If fingerprint file is not present, its not an error condition. It indicates that no files are blocked.
             if str(file_details.get("errorCode")) != "410" and not str(
                     file_details.get("errorMessage")).__contains__("do not exist"):
-                self.debug_print(consts.SEP_BLOCK_HASH_GET_DETAILS_ERROR.format(
+                self.debug_print(consts.SEP_BLOCK_HASH_GET_DETAILS_ERR.format(
                     name=fingerprint_filename
                 ))
                 return action_result.get_status(), None
@@ -706,8 +706,8 @@ class Sep14Connector(BaseConnector):
                 elif phantom.is_hostname(item):
                     search_key_field.append("computerName")
                 else:
-                    self.debug_print(consts.SEP_IP_HOSTNAME_VALIDATION_ERROR)
-                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_IP_HOSTNAME_VALIDATION_ERROR)
+                    self.debug_print(consts.SEP_IP_HOSTNAME_VALIDATION_ERR)
+                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_IP_HOSTNAME_VALIDATION_ERR)
 
         # Optional parameter
         timeout = param.get(consts.SEP_PARAM_TIMEOUT, 30)
@@ -901,8 +901,8 @@ class Sep14Connector(BaseConnector):
                 elif phantom.is_hostname(item):
                     search_key_field.append("computerName")
                 else:
-                    self.debug_print(consts.SEP_IP_HOSTNAME_VALIDATION_ERROR)
-                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_IP_HOSTNAME_VALIDATION_ERROR)
+                    self.debug_print(consts.SEP_IP_HOSTNAME_VALIDATION_ERR)
+                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_IP_HOSTNAME_VALIDATION_ERR)
 
         # Optional parameter
         timeout = param.get(consts.SEP_PARAM_TIMEOUT, 30)
@@ -1058,8 +1058,8 @@ class Sep14Connector(BaseConnector):
             if not fingerprint_file_id:
                 # If fingerprint file ID is not found in the response
                 if isinstance(file_resp_data, dict) and not file_resp_data.get("id"):
-                    self.debug_print(consts.SEP_BLOCK_HASH_GET_ID_ERROR)
-                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_BLOCK_HASH_GET_ID_ERROR)
+                    self.debug_print(consts.SEP_BLOCK_HASH_GET_ID_ERR)
+                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_BLOCK_HASH_GET_ID_ERR)
 
                 # Getting file ID of fingerprint list
                 fingerprint_file_id = file_resp_data.get("id")
@@ -1256,7 +1256,7 @@ class Sep14Connector(BaseConnector):
                 elif phantom.is_hostname(item):
                     search_key_field.append("computerName")
                 else:
-                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_IP_HOSTNAME_VALIDATION_ERROR)
+                    return action_result.set_status(phantom.APP_ERROR, consts.SEP_IP_HOSTNAME_VALIDATION_ERR)
 
             value_to_search = ip_hostname
 
